beb991bb9166c560e9774b75a5d26e24da90cb8a
Phil Tang <tangphillip@gmail.com>
2011-09-06 21:45:27 -0700

    Added a cap to the recall; now works with iframes

--- Tab Rescue.safariextension/hotkey.js
+++ Tab Rescue.safariextension/hotkey.js
@@ -1,10 +1,9 @@
-if (window.top === window) {
-	window.addEventListener("keydown", fireOnKeypress, false);
-	
-	function fireOnKeypress(e) {
-		if (e.shiftKey && (e.ctrlKey || e.metaKey) && e.keyCode == 84){
-			safari.self.tab.dispatchMessage("reopen");
-			e.preventDefault();
-		}
+window.addEventListener("keydown", fireOnKeypress, false);
+
+function fireOnKeypress(e) {
+	if (e.shiftKey && (e.ctrlKey || e.metaKey) && e.keyCode == 84){
+		safari.self.tab.dispatchMessage("reopen");
+		e.preventDefault();
 	}
 }
+
